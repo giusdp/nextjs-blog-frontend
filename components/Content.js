77d67f07536281395e2899371a9d2f8a5b959f38
giusdp <depalma.gsp@gmail.com>
2020-07-17 19:03:18 +0200

    Adjust spacing

--- components/Content.js
+++ components/Content.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Content({ children }) {
   return (
-    <div className="container w-full md:max-w-3xl mx-auto pt-20">
+    <div className="container w-full md:max-w-3xl mx-auto pt-20 mt-6">
       {children}
     </div>
   );
--- components/Nav.js
+++ components/Nav.js
@@ -11,7 +11,7 @@ const Nav = () => {
 
   return (
     <div>
-      <nav id="header" className="fixed w-full z-10 top-0">
+      <nav id="header" className="fixed w-full z-10 top-0 shadow-lg">
         <div className="w-full md:max-w-4xl mx-auto flex flex-wrap items-center justify-between mt-0 py-3">
           <div className="pl-4">
             <Link
