667642421f0079400e8fd49e1e27913ff5a2bae2
giusdp <depalma.gsp@gmail.com>
2020-07-17 20:45:25 +0200

    Add horizontal padding to contend body

--- components/ContentBody.js
+++ components/ContentBody.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function ContentBody({ children }) {
-  return <div className="py-6">{children}</div>;
+  return <div className="py-6 px-2 mx-auto">{children}</div>;
 }
