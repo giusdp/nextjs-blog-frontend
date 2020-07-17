0ddad4d23b8938d8254f2256eed25bc706ebb03b
giusdp <depalma.gsp@gmail.com>
2020-07-17 20:59:47 +0200

    Update styling

--- components/Title.js
+++ components/Title.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Title({ title }) {
   return (
-    <div className="w-full px-4 md:px-6 text-xl text-gray-800 leading-normal">
-      <h1 className=" font-bold break-normal text-gray-900 pt-6 pb-2 text-3xl md:text-4xl">
+    <div className="w-full px-4 md:px-6 leading-normal">
+      <div className="font-bold break-normal text-gray-900 pt-6 pb-2 text-4xl md:text-5xl">
         {title}
-      </h1>
+      </div>
     </div>
   );
 }
