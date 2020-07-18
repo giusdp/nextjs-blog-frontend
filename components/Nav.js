1ead9d1529cac053b901ecfe4c4937286b4e802b
giusdp <depalma.gsp@gmail.com>
2020-07-18 22:05:27 +0200

    Hide menu icon when query cateogories returns errors

--- components/Nav.js
+++ components/Nav.js
@@ -18,45 +18,43 @@ const Nav = () => {
   if (loading) return <ProgressBar />;
 
   return (
-    <div>
-      <nav
-        id="header"
-        className="lora fixed w-full z-10 top-0 shadow-lg bg-gray-100 bg-opacity-75"
-      >
-        <div className="w-full md:max-w-4xl mx-auto flex flex-wrap items-center justify-between mt-0 py-3">
-          <div className="pl-4">
-            <Link
-              href={{
-                pathname: "/",
-              }}
-            >
-              <a className="font-bold text-gray-900 text-base no-underline hover:no-underline font-extrabold text-xl">
-                Home
-              </a>
-            </Link>
-          </div>
+    <nav
+      id="header"
+      className="lora fixed w-full z-10 top-0 shadow-lg bg-gray-100 bg-opacity-75"
+    >
+      <div className="w-full md:max-w-4xl mx-auto flex flex-wrap items-center justify-between mt-0 py-3">
+        <div className="pl-4">
+          <Link
+            href={{
+              pathname: "/",
+            }}
+          >
+            <a className="font-bold text-gray-900 text-base no-underline hover:no-underline font-extrabold text-xl">
+              Home
+            </a>
+          </Link>
+        </div>
 
-          <div className="block md:hidden pr-4">
-            <button
-              onClick={() => setisMenuOpen(!isMenuOpen)}
-              id="nav-toggle"
-              className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-900 hover:border-teal-500 appearance-none focus:outline-none"
-            >
-              <svg
-                className="fill-current h-3 w-3"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+        {!error && data && (
+          <>
+            <div className="block md:hidden pr-4">
+              <button
+                onClick={() => setisMenuOpen(!isMenuOpen)}
+                id="nav-toggle"
+                className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-900 hover:border-teal-500 appearance-none focus:outline-none"
               >
-                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-              </svg>
-            </button>
-          </div>
-
-          <div className={toggleMenu(isMenuOpen)} id="nav-content">
-            <ul className="list-reset md:flex justify-end flex-1 items-center">
-              {!error &&
-                data &&
-                data.categories.map((category, i) => {
+                <svg
+                  className="fill-current h-3 w-3"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                </svg>
+              </button>
+            </div>
+            <div className={toggleMenu(isMenuOpen)} id="nav-content">
+              <ul className="list-reset md:flex justify-end flex-1 items-center">
+                {data.categories.map((category, i) => {
                   return (
                     <li key={category.id} className="mr-3">
                       <Link
@@ -73,11 +71,12 @@ const Nav = () => {
                     </li>
                   );
                 })}
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
+              </ul>
+            </div>
+          </>
+        )}
+      </div>
+    </nav>
   );
 };
 
